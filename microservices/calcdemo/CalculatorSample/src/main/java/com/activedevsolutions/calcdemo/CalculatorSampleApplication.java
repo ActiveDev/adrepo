495ce67c4a55cb53eacc6b8db2a9c7f9ee56ba9b
techguy <techguy@activedevsolutions.com>
2016-07-11 22:17:45 -0400

    Initial commit of a very basic calculator demo to demonstrate the creation of a microservice

---
+++ microservices/calcdemo/CalculatorSample/src/main/java/com/activedevsolutions/calcdemo/CalculatorSampleApplication.java
@@ -0,0 +1,12 @@
+package com.activedevsolutions.calcdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorSampleApplication.class, args);
+	}
+}
---
+++ microservices/calcdemo/CalculatorSample/src/main/java/com/activedevsolutions/calcdemo/model/CalcResult.java
@@ -0,0 +1,13 @@
+package com.activedevsolutions.calcdemo.model;
+
+public class CalcResult {
+	private long result;
+
+	public long getResult() {
+		return result;
+	}
+
+	public void setResult(long result) {
+		this.result = result;
+	}
+}
