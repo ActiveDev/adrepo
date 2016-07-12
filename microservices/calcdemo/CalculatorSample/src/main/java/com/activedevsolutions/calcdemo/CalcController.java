8a8515a9a8ed257cb374fa687310a2d9836a17d8
techguy <techguy@activedevsolutions.com>
2016-07-11 22:21:45 -0400

    Playing around with formatting

--- microservices/calcdemo/CalculatorSample/src/main/java/com/activedevsolutions/calcdemo/CalcController.java
+++ microservices/calcdemo/CalculatorSample/src/main/java/com/activedevsolutions/calcdemo/CalcController.java
@@ -14,9 +14,8 @@ import com.activedevsolutions.calcdemo.model.CalcError;
 import com.activedevsolutions.calcdemo.model.CalcResult;
 
 /**
- * Very simple demonstration of a REST controller that
- * returns a CalcResult. It also includes an exception handler 
- * that will return a CalcError.
+ * Very simple demonstration of a REST controller that returns a CalcResult. It
+ * also includes an exception handler that will return a CalcError.
  */
 @ControllerAdvice
 @RestController
@@ -24,52 +23,57 @@ public class CalcController {
 	/**
 	 * Very very very basic add method.
 	 * 
-	 * @param numberOne is the first value involved in the addition
-	 * @param numberTwo is used to add to the first value
+	 * @param numberOne
+	 *            is the first value involved in the addition
+	 * @param numberTwo
+	 *            is used to add to the first value
 	 * @return CalcResult holding the results of the addition
 	 */
 	@RequestMapping(value = "/add", method = RequestMethod.GET, produces = "application/json")
 	@ResponseBody
-    public CalcResult add(@RequestParam(value="number_one") int numberOne,
-    		            @RequestParam(value="number_two") int numberTwo) {
+	public CalcResult add(@RequestParam(value = "number_one") int numberOne,
+			@RequestParam(value = "number_two") int numberTwo) {
 		long result = numberOne + numberTwo;
 		CalcResult calcResult = new CalcResult();
 		calcResult.setResult(result);
-		
-        return calcResult;
-    }
-	
+
+		return calcResult;
+	}
+
 	/**
 	 * Very very very basic subtraction method.
 	 * 
-	 * @param numberOne is the first value involved in the subtraction
-	 * @param numberTwo is used to subtract from the first value
+	 * @param numberOne
+	 *            is the first value involved in the subtraction
+	 * @param numberTwo
+	 *            is used to subtract from the first value
 	 * @return CalcResult holding the results of the subtraction
 	 */
 	@RequestMapping(value = "/subtract", method = RequestMethod.GET, produces = "application/json")
 	@ResponseBody
-    public CalcResult subtract(@RequestParam(value="number_one") int numberOne,
-    		            @RequestParam(value="number_two") int numberTwo) {
+	public CalcResult subtract(@RequestParam(value = "number_one") int numberOne,
+			@RequestParam(value = "number_two") int numberTwo) {
 		int result = numberOne - numberTwo;
 		CalcResult calcResult = new CalcResult();
 		calcResult.setResult(result);
-		
-        return calcResult;
-    }
-	
+
+		return calcResult;
+	}
+
 	/**
-	 * Exception handler that will handle anything that derives
-	 * from the Exception class.
+	 * Exception handler that will handle anything that derives from the
+	 * Exception class.
 	 * 
-	 * @param e is the Exception object containing the exception thrown
+	 * @param e
+	 *            is the Exception object containing the exception thrown
 	 * @return CalcError containing the exception message
 	 */
-    @ResponseStatus(HttpStatus.BAD_REQUEST)  
-    @ExceptionHandler(value = Exception.class)  
-    public CalcError handleBaseException(Exception e) {
-    	CalcError calcError = new CalcError();
-    	calcError.setMessage(e.getMessage());
-    	
-        return calcError;  
-    }  
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(value = Exception.class)
+	public CalcError handleBaseException(Exception e) {
+		CalcError calcError = new CalcError();
+		calcError.setMessage(e.getMessage());
+
+		return calcError;
+	}
 }
